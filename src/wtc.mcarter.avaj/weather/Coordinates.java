5edaf20675432acb62e584a5898596f8ca852a3c
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-09-17 19:29:18 +0200

    implemented Coordinates class

--- src/wtc.mcarter.avaj/weather/Coordinates.java
+++ src/wtc.mcarter.avaj/weather/Coordinates.java
@@ -1,7 +1,25 @@
 package wtc.mcarter.avaj.weather;
 
-
-
 public class Coordinates {
+    private int _longitude;
+    private int _latitude;
+    private int _height;
 
+    public Coordinates(int longitude, int latitude, int height) {
+        _longitude = longitude;
+        _latitude = latitude;
+        _height = height;
+    }
+
+    public int getLongitude() {
+        return _longitude;
+    }
+
+    public int getLatitude() {
+        return _latitude;
+    }
+
+    public int getHeight() {
+        return _height;
+    }
 }
