311a15bcba1c8ab889f3ebcdf42544b0ee74e7ab
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-09-18 14:30:02 +0200

    added commented fix for Logger not being compiled

--- compile.bat
+++ compile.bat
@@ -1,5 +1,9 @@
 @echo off
 echo Finding sources...
 wsl -- find . -name *.java > sources.txt
+
+rem Fix for logger not being compiled
+rem echo ./wtc/mcarter/avaj/simulator/Logger.java >> sources.txt
+
 echo Compiling...
 javac -sourcepath @sources.txt
