b72a35f1ce5b81ed1b3bda6ae7259426b60fec0d
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-09-21 13:17:30 +0200

    changed find sources command to a windows command

--- compile.bat
+++ compile.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Finding sources...
-wsl -- find . -name *.java > sources.txt
+dir /s /b | findstr \.java$ > sources.txt
 
 rem Fix for logger not being compiled
-rem echo ./wtc/mcarter/avaj/simulator/Logger.java >> sources.txt
+rem echo .\wtc\mcarter\avaj\simulator\Logger.java>> sources.txt
 
 echo Compiling...
 javac -sourcepath @sources.txt
